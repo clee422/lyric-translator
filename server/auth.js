ac270c27eb1661023bb8f5e547df2a26c37c1cb0
clee422 <clee422@students.cs.ubc.ca>
2025-05-16 23:33:08 -0700

    Moved backend authorization routes to separate files

---
+++ server/auth.js
@@ -0,0 +1,71 @@
+import crypto from "crypto";
+
+export async function login(req, res) {
+    const generateRandomString = (length) => {
+        const possible =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const values = crypto.getRandomValues(new Uint8Array(length));
+        return values.reduce(
+            (acc, x) => acc + possible[x % possible.length],
+            ""
+        );
+    };
+
+    const state = generateRandomString(16);
+    const authUrl = new URL("https://accounts.spotify.com/authorize");
+
+    const authQueryParams = new URLSearchParams({
+        response_type: "code",
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: "streaming user-read-email user-read-private",
+        redirect_uri: "http://127.0.0.1:3000/auth/callback",
+        state: state,
+    });
+
+    authUrl.search = authQueryParams.toString();
+    res.redirect(authUrl.toString());
+}
+
+export async function callback(req, res) {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+
+    if (state === null) {
+        res.redirect("/");
+    } else {
+        const tokenResponse = await fetch(
+            "https://accounts.spotify.com/api/token",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    Authorization: `Basic ${new Buffer.from(
+                        process.env.SPOTIFY_CLIENT_ID +
+                            ":" +
+                            process.env.SPOTIFY_CLIENT_SECRET
+                    ).toString("base64")}`,
+                },
+                body: new URLSearchParams({
+                    code: code,
+                    redirect_uri: "http://127.0.0.1:3000/auth/callback",
+                    grant_type: "authorization_code",
+                }),
+            }
+        );
+        const tokenJson = await tokenResponse.json();
+        req.session.spotifyAccessToken = tokenJson.access_token;
+
+        res.redirect(`/`);
+    }
+}
+
+export async function token(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.json({
+        access_token: req.session.spotifyAccessToken,
+    });
+}
--- server/index.js
+++ server/index.js
@@ -3,9 +3,8 @@ import session from "express-session";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import crypto from "crypto";
-import { URLSearchParams } from "url";
-import { lyrics } from "./player/lyrics.js";
-import { translate } from "./player/translate.js";
+import { lyrics, translate } from "./playback.js";
+import { login, callback, token } from "./auth.js";
 
 const PORT = 5000;
 
@@ -25,75 +24,11 @@ app.use(
     })
 );
 
-app.get("/auth/login", async (req, res) => {
-    const generateRandomString = (length) => {
-        const possible =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        const values = crypto.getRandomValues(new Uint8Array(length));
-        return values.reduce(
-            (acc, x) => acc + possible[x % possible.length],
-            ""
-        );
-    };
+app.get("/auth/login", login);
 
-    const state = generateRandomString(16);
-    const authUrl = new URL("https://accounts.spotify.com/authorize");
+app.get("/auth/callback", callback);
 
-    const authQueryParams = new URLSearchParams({
-        response_type: "code",
-        client_id: process.env.SPOTIFY_CLIENT_ID,
-        scope: "streaming user-read-email user-read-private",
-        redirect_uri: "http://127.0.0.1:3000/auth/callback",
-        state: state,
-    });
-
-    authUrl.search = authQueryParams.toString();
-    res.redirect(authUrl.toString());
-});
-
-app.get("/auth/callback", async (req, res) => {
-    const code = req.query.code || null;
-    const state = req.query.state || null;
-
-    if (state === null) {
-        res.redirect("/");
-    } else {
-        const tokenResponse = await fetch(
-            "https://accounts.spotify.com/api/token",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    Authorization: `Basic ${new Buffer.from(
-                        process.env.SPOTIFY_CLIENT_ID +
-                            ":" +
-                            process.env.SPOTIFY_CLIENT_SECRET
-                    ).toString("base64")}`,
-                },
-                body: new URLSearchParams({
-                    code: code,
-                    redirect_uri: "http://127.0.0.1:3000/auth/callback",
-                    grant_type: "authorization_code",
-                }),
-            }
-        );
-        const tokenJson = await tokenResponse.json();
-        req.session.accessToken = tokenJson.access_token;
-
-        res.redirect(`/`);
-    }
-});
-
-app.get("/auth/token", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.json({
-        access_token: req.session.accessToken,
-    });
-});
+app.get("/auth/token", token);
 
 app.get("/player/lyrics", lyrics);
 
